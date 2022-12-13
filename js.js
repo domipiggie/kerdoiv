5d023dec436665e7a9f4433cce604274768769b7
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2022-12-13 10:56:58 +0100

    kerdeseksz update
    kerdesex update

--- js.js
+++ js.js
@@ -4,7 +4,11 @@ function checkAnswers(){
     let tyreTrue = document.getElementById("tyreT").checked;
     let tyreFalse = document.getElementById("tyreF").checked;
 
-    if (haloYear==""||sebWDCcount==""||(tyreFalse==false&&tyreTrue==false)){
+    let kerdes1 = document.getElementById("kerdes1").value;
+    let kerdes2 = document.getElementById("kerdes2").value;
+    let kerdes3 = document.getElementById("kerdes3").value;
+
+    if (haloYear==""||sebWDCcount==""||kerdes1==""||kerdes2==""||kerdes3==""||(tyreFalse==false&&tyreTrue==false)){
         alert("Kérem töltse ki az összes mezőt");
         return false;
     }
