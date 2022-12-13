d26293ac5fbf7ffe251035c6d0ddd3325db678fd
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2022-12-13 10:50:26 +0100

    mezőkitöltés ellenőrző

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
 function checkAnswers(){
+    let haloYear = document.getElementById("haloYear").value;
+    let sebWDCcount = document.getElementById("sebWDC").value;
+    let tyreTrue = document.getElementById("tyreT").checked;
+    let tyreFalse = document.getElementById("tyreF").checked;
+
+    if (haloYear==""||sebWDCcount==""||(tyreFalse==false&&tyreTrue==false)){
+        alert("Kérem töltse ki az összes mezőt");
+        return false;
+    }
+
     
 }
