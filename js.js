5cf992e834ad6413a01c4600ef7a743298c70cde
domipiggie <basti.domonkos.2020i@bankitatabanya.hu>
2022-12-13 11:37:40 +0100

    hibaüzenet updated

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ function checkAnswers(){
     let kerdes3 = document.getElementById("kerdes3").value;
 
     if (haloYear==""||sebWDCcount==""||kerdes1==""||kerdes2==""||kerdes3==""||(tyreFalse==false&&tyreTrue==false)){
-        alert("Kérem töltse ki az összes mezőt");
+        alert("Kérem töltse ki az összes mezőt (ne legyé marky)");
         return false;
     }
 
